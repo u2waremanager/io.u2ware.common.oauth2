8fa79cf17d030f2011d77732a335acec91df3039
u2waremanager@gamil.com <u2waremanager@gamil.com>
2025-12-18 12:12:49 +0900

    u2ware-common-oauth2

---
+++ src/main/java/io/u2ware/common/oauth2/jwt/JwtDecoderBuilder.java
@@ -0,0 +1,77 @@
+package io.u2ware.common.oauth2.jwt;
+
+import java.nio.file.Path;
+import java.security.interfaces.RSAPublicKey;
+import java.util.stream.IntStream;
+
+import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
+import org.springframework.core.io.Resource;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtException;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.util.ObjectUtils;
+
+import com.nimbusds.jose.jwk.RSAKey;
+
+import io.u2ware.common.oauth2.crypto.CryptoKeyFiles;
+import io.u2ware.common.oauth2.crypto.JoseKeyEncryptor;
+import io.u2ware.common.oauth2.crypto.JoseKeyGenerator;
+
+public class JwtDecoderBuilder {
+    
+    public JwtDecoder build(OAuth2ResourceServerProperties properties) {
+
+        try{
+            Resource publicKeyLocation = properties.getJwt().getPublicKeyLocation();
+            String jwkSetUri = properties.getJwt().getJwkSetUri();
+
+            if(! ObjectUtils.isEmpty(publicKeyLocation)) {
+                // logger.info("NimbusJwtDecoder by publicKeyLocation");
+                Path path = Path.of(publicKeyLocation.getURI());
+                RSAPublicKey publicKey = CryptoKeyFiles.readRSAPublicKey(path);
+                return NimbusJwtDecoder.withPublicKey(publicKey).build();
+            }
+
+            if(! ObjectUtils.isEmpty(jwkSetUri)) {
+                // logger.info("NimbusJwtDecoder by jwkSetUri");
+                return NimbusJwtDecoder.withJwkSetUri(jwkSetUri).build();
+            }
+
+            RSAKey rsaKey = JoseKeyGenerator.generateRsa();
+            
+            
+            System.err.println("\n");
+            IntStream.range(1, 6).forEach(i->{
+
+                String name = "test_account_0"+i;
+                Jwt jwt = JoseKeyEncryptor.encrypt(rsaKey, claims->{
+                    claims.put("sub", name);
+                    claims.put("email", name);
+                    claims.put("name", name);
+                });
+                System.err.println("#VITE_API_TOKEN="+jwt.getTokenValue());
+            });
+            System.err.println("\n");
+
+            RSAPublicKey publicKey = rsaKey.toRSAPublicKey();
+            return NimbusJwtDecoder.withPublicKey(publicKey).build();
+
+        }catch(Exception e){
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    private JwtDecoderBuilder(){
+    }
+
+    private static JwtDecoderBuilder instance;
+
+    public static JwtDecoderBuilder getInstance(){
+        if(instance == null) {
+            instance = new JwtDecoderBuilder();
+        }
+        return instance;
+    }
+}
--- src/main/java/io/u2ware/common/oauth2/webmvc/AuthorizationEndpoints.java
+++ src/main/java/io/u2ware/common/oauth2/webmvc/AuthorizationEndpoints.java
@@ -32,155 +32,156 @@ import jakarta.servlet.http.HttpServletRequest;
 @Controller
 public class AuthorizationEndpoints {
 
-    // protected Log logger = LogFactory.getLog(getClass());
+    protected Log logger = LogFactory.getLog(getClass());
 
-    // @Autowired
-    // public ObjectMapper objectMapper;
-
-
-    // @GetMapping(value = "/oauth2/userinfo")
-    // public @ResponseBody ResponseEntity<Object> oauth2UserInfo(Authentication authentication) {
-
-    //     logger.info("oauth2UserInfo : "+authentication);
-    //     try {
-    //         Jwt jwt = AuthenticationContext.authenticationToken(authentication);
-
-    //         Collection<GrantedAuthority> securityAuthorities = AuthenticationContext.authorities(authentication);
-    //         logger.info("securityAuthorities : "+securityAuthorities);
-
-    //         Collection<GrantedAuthority> jwtAuthorities = AuthenticationContext.authorities(jwt);
-    //         logger.info("jwtAuthorities : "+jwtAuthorities);
+    @Autowired
+    public ObjectMapper objectMapper;
 
 
-    //         Set<String> responseAuthorities = new HashSet<>();
-    //         if(securityAuthorities != null) {
-    //             responseAuthorities.addAll(securityAuthorities.stream().map(GrantedAuthority::getAuthority).collect(Collectors.toSet()));
-    //         }
-    //         if(jwtAuthorities != null) {
-    //             responseAuthorities.addAll(jwtAuthorities.stream().map(GrantedAuthority::getAuthority).collect(Collectors.toSet()));
-    //         }
-    //         logger.info("responseAuthorities : "+responseAuthorities);
+    @GetMapping(value = "/oauth2/userinfo")
+    public @ResponseBody ResponseEntity<Object> oauth2UserInfo(Authentication authentication) {
 
-    //         Map<String,Object> claims = jwt.getClaims();
-    //         Map<String,Object> responseClaims = new HashMap<>(claims);
-    //         responseClaims.put("authorities", responseAuthorities);
+        logger.info("oauth2UserInfo : "+authentication);
+        try {
+            Jwt jwt = AuthenticationContext.authenticationToken(authentication);
 
-    //         @SuppressWarnings("unchecked")
-    //         Map<String,Object> responseJwt = objectMapper.convertValue(jwt, Map.class);
-    //         responseJwt.put("claims", responseClaims);
+            Collection<GrantedAuthority> securityAuthorities = AuthenticationContext.authorities(authentication);
+            logger.info("securityAuthorities : "+securityAuthorities);
 
-    //         return ResponseEntity.ok(responseJwt);
-
-    //     } catch (Exception e) {
-    //         // e.printStackTrace();
-    //         return ResponseEntity.status(HttpStatusCode.valueOf(401)).body(e.getMessage());
-    //     }
-    // }
-
-    // @GetMapping(value = "/oauth2/providers")
-    // public @ResponseBody ResponseEntity<Object> oauth2Providers(HttpServletRequest request) {
-
-    //     if(JoseKeyCodec.unavailable()){
-    //         return ResponseEntity.status(HttpStatusCode.valueOf(404)).build();
-    //     }
-
-    //     try{
-    //         UriComponents uri = ServletUriComponentsBuilder.fromContextPath(request)
-    //         .path("/oauth2/login")
-    //         .queryParam("provider", "{provider}")
-    //         .queryParam("callback", "{callback}")
-    //         .build();
-
-    //         ArrayList<Object> providers = new ArrayList<>();
-    //         Map<String, Object> provider = new HashMap<>();
-    //         provider.put("name", "Test Oauth2");
-    //         provider.put("uri", uri.toUriString());
-    //         providers.add(provider);
-
-    //         return ResponseEntity.ok(providers);
-
-    //     }catch(Exception e){
-    //         e.printStackTrace();
-    //         return ResponseEntity.status(HttpStatusCode.valueOf(404)).build();
-    //     }
-    // }
+            Collection<GrantedAuthority> jwtAuthorities = AuthenticationContext.authorities(jwt);
+            logger.info("jwtAuthorities : "+jwtAuthorities);
 
 
-    // @PostMapping(value = "/oauth2/login", params = { "provider"})
-    // public @ResponseBody ResponseEntity<Object> oauth2Login(HttpServletRequest request,
-    //         @RequestParam("provider") String provider) {
+            Set<String> responseAuthorities = new HashSet<>();
+            if(securityAuthorities != null) {
+                responseAuthorities.addAll(securityAuthorities.stream().map(GrantedAuthority::getAuthority).collect(Collectors.toSet()));
+            }
+            if(jwtAuthorities != null) {
+                responseAuthorities.addAll(jwtAuthorities.stream().map(GrantedAuthority::getAuthority).collect(Collectors.toSet()));
+            }
+            logger.info("responseAuthorities : "+responseAuthorities);
 
-    //     if(JoseKeyCodec.unavailable()){
-    //         return ResponseEntity.status(HttpStatusCode.valueOf(404)).build();
-    //     }
+            Map<String,Object> claims = jwt.getClaims();
+            Map<String,Object> responseClaims = new HashMap<>(claims);
+            responseClaims.put("authorities", responseAuthorities);
 
-    //     try{
-    //         Jwt jwt = JoseKeyCodec.getInstance().encrypt((claims)->{
-    //             claims.put("sub", provider);
-    //             claims.put("email", provider);
-    //             claims.put("name", provider);
-    //         });    
-    //         String idToken = jwt.getTokenValue();            
-    //         return ResponseEntity.ok(idToken);//.status(HttpStatus.OK).c.headers(headers).build();
+            @SuppressWarnings("unchecked")
+            Map<String,Object> responseJwt = objectMapper.convertValue(jwt, Map.class);
+            responseJwt.put("claims", responseClaims);
 
-    //     }catch(Exception e){
-    //         e.printStackTrace();
-    //         return ResponseEntity.status(HttpStatusCode.valueOf(404)).build();
-    //     }
-    // }
+            return ResponseEntity.ok(responseJwt);
+
+        } catch (Exception e) {
+            // e.printStackTrace();
+            return ResponseEntity.status(HttpStatusCode.valueOf(401)).body(e.getMessage());
+        }
+    }
+
+    @GetMapping(value = "/oauth2/providers")
+    public @ResponseBody ResponseEntity<Object> oauth2Providers(HttpServletRequest request) {
+
+        // if(JoseKeyCodec.unavailable()){
+        //     return ResponseEntity.status(HttpStatusCode.valueOf(404)).build();
+        // }
+
+        try{
+            UriComponents uri = ServletUriComponentsBuilder.fromContextPath(request)
+            .path("/oauth2/login")
+            .queryParam("provider", "{provider}")
+            .queryParam("callback", "{callback}")
+            .build();
+
+            ArrayList<Object> providers = new ArrayList<>();
+            Map<String, Object> provider = new HashMap<>();
+            provider.put("name", "Test Oauth2");
+            provider.put("uri", uri.toUriString());
+            providers.add(provider);
+
+            return ResponseEntity.ok(providers);
+
+        }catch(Exception e){
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatusCode.valueOf(404)).build();
+        }
+    }
+
+
+    @PostMapping(value = "/oauth2/login", params = { "provider"})
+    public @ResponseBody ResponseEntity<Object> oauth2Login(HttpServletRequest request,
+            @RequestParam("provider") String provider) {
+
+        // if(JoseKeyCodec.unavailable()){
+        //     return ResponseEntity.status(HttpStatusCode.valueOf(404)).build();
+        // }
+
+        try{
+            // Jwt jwt = JoseKeyCodec.getInstance().encrypt((claims)->{
+            //     claims.put("sub", provider);
+            //     claims.put("email", provider);
+            //     claims.put("name", provider);
+            // });    
+            Jwt jwt = null;
+            String idToken = jwt.getTokenValue();            
+            return ResponseEntity.ok(idToken);//.status(HttpStatus.OK).c.headers(headers).build();
+
+        }catch(Exception e){
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatusCode.valueOf(404)).build();
+        }
+    }
 
 
 
-    // @GetMapping(value = "/oauth2/login", params = { "provider", "callback" })
-    // public @ResponseBody ResponseEntity<Object> oauth2Login(HttpServletRequest request,
-    //         @RequestParam("provider") String provider, @RequestParam("callback") String callback) {
+    @GetMapping(value = "/oauth2/login", params = { "provider", "callback" })
+    public @ResponseBody ResponseEntity<Object> oauth2Login(HttpServletRequest request,
+            @RequestParam("provider") String provider, @RequestParam("callback") String callback) {
 
-    //     if(JoseKeyCodec.unavailable()){
-    //         return ResponseEntity.status(HttpStatusCode.valueOf(404)).build();
-    //     }
+        // if(JoseKeyCodec.unavailable()){
+        //     return ResponseEntity.status(HttpStatusCode.valueOf(404)).build();
+        // }
 
-    //     try{
-    //         Jwt jwt = JoseKeyCodec.getInstance().encrypt((claims)->{
-    //             claims.put("sub", provider);
-    //             claims.put("email", provider);
-    //             claims.put("name", provider);
-    //         });
-            
-    //         String userinfo = ServletUriComponentsBuilder
-    //             .fromContextPath(request)
-    //             .path("/oauth2/userinfo")
-    //             .build()
-    //             .toString();
-    //         String tokenType = "Bearer";
-    //         String principalName = provider;
-    //         String accessToken = jwt.getTokenValue();
-    //         String idToken = jwt.getTokenValue();
+        try{
+            // Jwt jwt = JoseKeyCodec.getInstance().encrypt((claims)->{
+            //     claims.put("sub", provider);
+            //     claims.put("email", provider);
+            //     claims.put("name", provider);
+            // });
+            Jwt jwt = null;
+            String userinfo = ServletUriComponentsBuilder
+                .fromContextPath(request)
+                .path("/oauth2/userinfo")
+                .build()
+                .toString();
+            String tokenType = "Bearer";
+            String principalName = provider;
+            String accessToken = jwt.getTokenValue();
+            String idToken = jwt.getTokenValue();
     
-    //         UriComponents location = UriComponentsBuilder
-    //                 .fromUriString(callback)
-    //                 .queryParam("username", principalName)
-    //                 .queryParam("raw_info", userinfo)
-    //                 .queryParam("raw_token", accessToken)
-    //                 .queryParam("token_type", tokenType)
-    //                 .queryParam("id_token", idToken)
-    //                 .build();
+            UriComponents location = UriComponentsBuilder
+                    .fromUriString(callback)
+                    .queryParam("username", principalName)
+                    .queryParam("raw_info", userinfo)
+                    .queryParam("raw_token", accessToken)
+                    .queryParam("token_type", tokenType)
+                    .queryParam("id_token", idToken)
+                    .build();
     
-    //         HttpHeaders headers = new HttpHeaders();
-    //         headers.setLocation(location.toUri());
-    //         return ResponseEntity.status(HttpStatus.MOVED_PERMANENTLY).headers(headers).build();
+            HttpHeaders headers = new HttpHeaders();
+            headers.setLocation(location.toUri());
+            return ResponseEntity.status(HttpStatus.MOVED_PERMANENTLY).headers(headers).build();
     
-    //     }catch(Exception e){
-    //         e.printStackTrace();
-    //         return ResponseEntity.status(HttpStatusCode.valueOf(404)).build();
-    //     }
-    // }
+        }catch(Exception e){
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatusCode.valueOf(404)).build();
+        }
+    }
 
-    // @GetMapping(value = "/oauth2/logout")
-    // public @ResponseBody ResponseEntity<Object> oauth2Logout() {       
+    @GetMapping(value = "/oauth2/logout")
+    public @ResponseBody ResponseEntity<Object> oauth2Logout() {       
 
-    //     if(JoseKeyCodec.unavailable()){
-    //         return ResponseEntity.status(HttpStatusCode.valueOf(404)).build();
-    //     }        
-    //     return ResponseEntity.ok("OK");
-    // }    
+        // if(JoseKeyCodec.unavailable()){
+        //     return ResponseEntity.status(HttpStatusCode.valueOf(404)).build();
+        // }        
+        return ResponseEntity.ok("OK");
+    }    
 }
